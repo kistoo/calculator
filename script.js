369b52b54e3c88cdf817fbe708bfd07eff023187
kistoo <tomio.nakamura15@gmail.com>
2022-10-20 18:25:13 -0500

    Add filter display
    In process delete case zero for initButton

--- script.js
+++ script.js
@@ -30,26 +30,42 @@ function operate (operation, number1, number2) {
 }
 
 //display interaction with buttons
-function display(button){
-    if (button === "CA") {
+function display(input){
+    if (input === "CA") {
         displayContent = "0";
-    } else if (button === "C") {
+    } else if (input === "C") {
         displayContent = displayContent.substring(0,displayContent.length-1);
-    } else if (button === "=") {
+    } else if (input === "=") {
         displayContent = `${result}`;
     } else {
-        displayContent += button;
+        filterDisplay(input);
     }
     displayDiv.textContent = displayContent;
 }
 
-//Create array with buttons as objects
+//create a filter for display
+function filterDisplay(input) {
+    const button = inputValues.find(item => item.value===input);
+    switch(button.type) {
+        case "number":
+            if (button.value !== 0) {
+                displayContent = `${button.value}`;
+            } else {
+                displayContent+=`${button.value}`;
+            }
+        case "operation":
+            displayContent+=`${button.value}`;
+    } 
+}
+    
+
+//create array with buttons as objects
 function initButton(value) {
     const button = {
         value,
     }
     //numbers
-    if ((value>0) && 
+    if ((value>=0) && 
         (value<10)) {
             button.type = "number";
     } else if ((value==="+") || //operations
@@ -57,8 +73,6 @@ function initButton(value) {
         (value==="*") || 
         (value==="/")) {
             button.type = "op";
-    } else if (value===0) { //zero
-        button.type = "zero";
     }
     //CA, C and = dont need type
     inputValues.push(button);
@@ -76,5 +90,3 @@ buttons.forEach(button => {
         display(button.textContent));
         initButton(button.textContent);
 });
-
-console.log(inputValues);
