edec10aa7a4a66e27e34b5fbfc624a88c319bb15
kistoo <tomio.nakamura15@gmail.com>
2022-10-20 15:55:08 -0500

    Add basic operations

---
+++ script.js
@@ -0,0 +1,16 @@
+//basic operations
+function sum (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
